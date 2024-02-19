e7854fa11c1ff47c9d3f7e23a6a04cb1e6d1c9bc
ch1llc0d3 <chillcrib7@gmail.com>
2024-02-19 12:18:14 -0300

    outputs.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region = aws.region
 }
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
-  instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+  ami_value = var.ami_value # replace this
+  instance_type_value = var.instance_type_value
+  subnet_id_value =  var.subnet_id_value
 }
