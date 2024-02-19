84dee289b7eb22fe3030d833d9f5e4634e10fb1c
ch1llc0d3 <chillcrib7@gmail.com>
2024-02-19 12:36:56 -0300

    modularization, in day-3, need to modularize to make work environment easier for everybody

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -1,10 +1,11 @@
 provider "aws" {
-  region = aws.region
+    region = "us-east-1"
 }
 
 module "ec2_instance" {
-  source = "./modules/ec2_instance"
-  ami_value = var.ami_value # replace this
-  instance_type_value = var.instance_type_value
-  subnet_id_value =  var.subnet_id_value
+    source = "./modules/ec2_instance"
+    region = "us-east-1"
+    ami_value = "ami-0c7217cdde317cfec"
+    instance_type_value = "t2.micro"
+    subnet_id_value = "subnet-0e0551a75e927f679"
 }
--- Day-3/modules/ec2_instance/variables.tf
+++ Day-3/modules/ec2_instance/variables.tf
@@ -1,11 +1,15 @@
+variable "region" {
+#   type = string
+}
+
 variable "ami_value" {
-    description = "value for the ami"
+#   type = string
 }
 
 variable "instance_type_value" {
-    description = "value for instance_type"
+#   type = string
 }
 
 variable "subnet_id_value" {
-    description = "value for the subnet_id"
+#   type = string
 }
